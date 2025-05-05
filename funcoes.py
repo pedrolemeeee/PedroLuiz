b331bb375d603e73033db50d50ebcf71621ddad2
pedrolemeeee <pedrolcol@al.insper.edu.br>
2025-05-05 20:58:36 -0300

    ex4 Pedro

--- funcoes.py
+++ funcoes.py
@@ -17,3 +17,10 @@ def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
     dados_rolados.append(dados_no_estoque[dado_para_remover])
     dados_no_estoque = [dado for i, dado in enumerate(dados_no_estoque) if i != dado_para_remover]
     return [dados_rolados, dados_no_estoque]
+
+def calcula_pontos_regra_simples(dados):
+    pontos = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+    for numero in dados:
+        if numero in pontos:
+            pontos[numero] = pontos[numero] + numero
+    return pontos
