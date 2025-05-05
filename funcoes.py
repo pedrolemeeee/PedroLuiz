b8e04fe014d423f16314b56a9fcf9b53a3662ff7
pedrolemeeee <pedrolcol@al.insper.edu.br>
2025-05-05 20:31:00 -0300

    ex2 pedro

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,9 @@ def rolar_dados(quantidade):
         resultado = random.randint(1, 6)
         resultados.append(resultado)
     return resultados
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    dado = dados_rolados[dado_para_guardar]
+    dados_no_estoque.append(dado)
+    dados_rolados.pop(dado_para_guardar)
+    return [dados_rolados, dados_no_estoque]
